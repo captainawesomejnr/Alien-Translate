0cdcd96dd1e32b39fb3d0c5d7b084609ade48fae
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2022-09-19 14:47:12 +1000

    Add files via upload

--- Alien Translate.py
+++ Alien Translate.py
@@ -1,5 +1,5 @@
-print("Alien Translate (v1.0.0)")
-AlienDictionary = {"we": "vorag", "come": "thang", "in": "zon", "peace": "argh", "hello": "kodar", "can": "znak", "i": "az", "borrow": "liftit", "some": "zum", "rocket": "upgoman", "fuel": "kakboom", "please": "selpin", "don't": "baaaaaaaaarn", "shoot": "flabil", "welcome": "unkip", "our": "mandig", "new": "brang", "alien": "marangin", "overlords": "bap", "earth": "Picom", "planet": "bumpis", "hello": "wregaxez", "toilet": "tarrg", "came": "cig", "a": "iv", "an": "ivin", "to": "tiik"}
+print("Alien Translate (v1.0.1)")
+AlienDictionary = {"we": "vorag", "come": "thang", "in": "zon", "peace": "argh", "hello": "kodar", "can": "znak", "i": "az", "borrow": "liftit", "some": "zum", "rocket": "upgoman", "fuel": "kakboom", "please": "selpin", "don't": "baaaaaaaaarn", "shoot": "flabil", "welcome": "unkip", "our": "mandig", "new": "brang", "alien": "marangin", "overlords": "bap", "earth": "Picom", "planet": "bumpis", "hello": "wregaxez", "toilet": "tarrg", "came": "cig", "a": "iv", "an": "ivin", "to": "tiik", "lawn": "lavyurn", "cut": "tivun", "lawnmower": "lavyurnamin", "mover": "min", "earmuffs": "vroomaprow"}
 EnglishPhrase = input("Please enter something in English to translate: ")
 EnglishWords = EnglishPhrase.lower().split()
 AlienWords = []
