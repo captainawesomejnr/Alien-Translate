e9741d650503f3bd0570164d8960df25c1a804ba
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2022-09-19 14:03:14 +1000

    Add files via upload

---
+++ Alien Translate.py
@@ -0,0 +1,11 @@
+print("Alien Translate (v1.0.0)")
+AlienDictionary = {"we": "vorag", "come": "thang", "in": "zon", "peace": "argh", "hello": "kodar", "can": "znak", "i": "az", "borrow": "liftit", "some": "zum", "rocket": "upgoman", "fuel": "kakboom", "please": "selpin", "don't": "baaaaaaaaarn", "shoot": "flabil", "welcome": "unkip", "our": "mandig", "new": "brang", "alien": "marangin", "overlords": "bap", "earth": "Picom", "planet": "bumpis", "hello": "wregaxez", "toilet": "tarrg", "came": "cig", "a": "iv", "an": "ivin", "to": "tiik"}
+EnglishPhrase = input("Please enter something in English to translate: ")
+EnglishWords = EnglishPhrase.lower().split()
+AlienWords = []
+for word in EnglishWords:
+    if word in AlienDictionary:
+        AlienWords.append(AlienDictionary[word])
+    else:
+        AlienWords.append(word)
+print("In Alien, say:", " ".join(AlienWords))
